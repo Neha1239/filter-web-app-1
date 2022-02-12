cf086ae9abf454b2c4297d957e836f2e703f7b19
Neha1239 <86868752+Neha1239@users.noreply.github.com>
2022-02-12 12:17:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload() {
-
+lipstick_Image=loadImage('https://i.postimg.cc/JhzrJ4Z8/l1.png');
 }
 function setup() {
 canvas=createCanvas(300,300);
@@ -11,7 +13,8 @@ poseNet=ml5.poseNet(video,modelLoaded);
 poseNet.on('pose',gotPoses);
 }
 function draw() {
-Image(video,0,0,300,300)
+image(video,0,0,300,300)
+image(lipstick_Image,noseX,noseY,30,30);
 }
 function take_snapshot() {
     save('myFilterImage.png')
@@ -22,6 +25,8 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length>0) {
         console.log(results);
+        noseX=results[0].pose.nose.x-15;
+        noseY=results[0].pose.nose.y+15;
         console.log("nose x = "+results[0].pose.nose.x);
         console.log("nose y = "+results[0].pose.nose.y);
     }
